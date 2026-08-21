07e4aa94b9f50e3197b2c17c693e3815afaad17a
fm <1286670508@qq.com>
2026-08-21 13:20:31 +0800

    Delete reid directory

--- reid/feature_extraction/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from .cnn import extract_cnn_feature, extract_pcb_feature
-from .database import FeatureDatabase
-
-__all__ = [
-    'extract_cnn_feature', 'extract_pcb_feature',
-    'FeatureDatabase',
-]
